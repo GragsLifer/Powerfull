743cd9e0f615b2c2f47fdbe26fe9d4e0fa7881aa
GragsLifer <buildinto7@gmail.com>
2025-07-05 09:38:22 +0900

    Update File1.lua

--- File1.lua
+++ File1.lua
@@ -11,7 +11,7 @@ obj430811.BrickColor = BrickColor.new("Medium stone grey")
 obj430811.Parent = game.Workspace
 local obj529986 = Instance.new("ModuleScript")
 obj529986.Name = "Cum"
-obj529986.Source = "local cumshots = {}\nlocal cumgravity = 60\n"
+obj529986.Source = "local cumshots = {}\nlocal cumgravity = 60"
 
 local rng = Random.new()\
 \
